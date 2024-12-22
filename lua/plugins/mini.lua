487cf35688f9b55a4da9936c9316087a1f787fce
troyfischer <troytfischer@gmail.com>
2024-12-22 14:48:01 -0600

    remove unused lazy configs

--- lua/plugins/mini.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "echasnovski/mini.nvim",
-        config = function()
-            local status_line = require "mini.statusline"
-            status_line.setup({use_icons = true})
-        end
-    }
-}
